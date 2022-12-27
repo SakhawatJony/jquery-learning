34e113ff356db77c01a4a18c497873e817ba09d6
Sakhawat Hosen <sakhawathosen320@gmail.com>
2022-12-28 00:37:58 +0600

    jquery learning

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+$( ".hide" ).click(function() {
+    $('.container-title').hide();
+  });
+$( ".show" ).click(function() {
+    $('.container-title').show();
+  });
+$( ".remove" ).click(function() {
+    $('.container-title').remove();
+  });
+$( ".empty" ).click(function() {
+    $('.container-title').empty();
+  });
